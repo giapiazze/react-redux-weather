99ac184b01e609f7d941f471fcf1352d8fcaff3b
Giacomo PIazzesi <giapiazze@gmail.com>
2017-04-30 02:40:49 +0200

    20170430 - Ci siamo quasi

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -11,10 +11,11 @@ class WeatherList extends Component {
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
 		const { lon, lat } = cityData.city.coord;
+		console.log('City: ', name);
 
 		return (
-			<tr key={name}>
-				<td><GoogleMap lon={lon} lat={lat} /></td>
+			<tr key={Date.now()}>
+				<td>{name}</td>
 				<td><Chart data={temps} color="orange" units="K" /></td>
 				<td><Chart data={pressures} color="green" units="hPa" /></td>
 				<td><Chart data={humidities} color="black" units="%" /></td>
